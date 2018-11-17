03b68331c0be290e0b9582e00cefaf831d5c24cc
yasinkbas <yasin.kbas12@gmail.com>
2018-11-17 15:16:07 +0300

    created welcome screen

---
+++ 2 Core IOS Auto Layout&Segues/Swoosh/app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Yasin on 11/17/18.
+//  Copyright © 2018 Yasin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
