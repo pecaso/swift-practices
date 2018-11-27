459e6ecca4884e358f92798fc53f8924d5e893a6
yasinkbas <yasin.kbas12@gmail.com>
2018-11-27 20:20:49 +0300

    added calcBtn in window-shopper

--- 2 Core IOS Auto Layout&Segues/window-shopper/window-shopper/Controller/MainVC.swift
+++ 2 Core IOS Auto Layout&Segues/window-shopper/window-shopper/Controller/MainVC.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView  = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
+    }
+    
+    @objc func calculate(){
+        print("we got here")
     }
 
 
--- 2 Core IOS Auto Layout&Segues/window-shopper/window-shopper/View/CurrencyTxtField.swift
+++ 2 Core IOS Auto Layout&Segues/window-shopper/window-shopper/View/CurrencyTxtField.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
    
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+        
+        
+    }
+    
+    
+    
+    
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.2489833048)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -24,8 +37,6 @@ class CurrencyTxtField: UITextField {
             attributedPlaceholder = place
             
         }
-        
-        
     }
 
 }
