86b7a59a4c65ebc54ed9a4b775d9538e50a80a0c
yasinkbas <yasin.kbas12@gmail.com>
2018-11-30 15:06:57 +0300

    added project creation & IBOutlets

---
+++ 3 Tables & Data/coder-swag/coder-swag/Controller/CategoriesVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Yasin on 11/30/18.
+//  Copyright © 2018 Yasin. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
---
+++ 3 Tables & Data/coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Yasin on 11/30/18.
+//  Copyright © 2018 Yasin. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
