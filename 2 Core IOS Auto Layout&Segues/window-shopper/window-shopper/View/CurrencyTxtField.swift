17ae3e7eac7ded285499bca15256080aff914553
yasinkbas <yasin.kbas12@gmail.com>
2018-11-30 14:11:37 +0300

    added draw with drawRect

--- 2 Core IOS Auto Layout&Segues/window-shopper/window-shopper/View/CurrencyTxtField.swift
+++ 2 Core IOS Auto Layout&Segues/window-shopper/window-shopper/View/CurrencyTxtField.swift
@@ -10,6 +10,22 @@ import UIKit
 
 @IBDesignable
 class CurrencyTxtField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 0.7985070634)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
 
     override func prepareForInterfaceBuilder() {
         customizeView()
@@ -27,6 +43,7 @@ class CurrencyTxtField: UITextField {
         layer.cornerRadius = 5.0
         textAlignment = .center
         textColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        clipsToBounds = true
         
         
         if let p = placeholder {
