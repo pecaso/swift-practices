02752ab4ae55298b77714767b89250880798595d
yasinkbas <yasin.kbas12@gmail.com>
2018-12-01 13:33:35 +0300

    added collectionView

---
+++ 3 Tables & Data/coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Yasin on 12/1/18.
+//  Copyright © 2018 Yasin. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+    
+}
