0ab76f43949552992cb0c036dc84680c29e10054
yasinkbas <yasin.kbas12@gmail.com>
2018-12-03 21:39:52 +0300

    end of smack project setup

---
+++ 4 Network & API & REST/Smack/Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Yasin on 12/3/18.
+//  Copyright © 2018 Yasin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
