27a92c73af47e41c680804cbc9c3792de3683dfe
yasinkbas <yasin.kbas12@gmail.com>
2018-12-18 17:00:27 +0300

    added splitter project

---
+++ 6 Splitter/Splits/Splits/MasterViewController.swift
@@ -0,0 +1,67 @@
+//
+//  MasterViewController.swift
+//  Splits
+//
+//  Created by Yasinkbas on 12/18/18.
+//  Copyright © 2018 Yasin. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+
+    // MARK: - Segues
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                
+                let controller = (segue.destination as! UINavigationController).topViewController as! ImagePresentationVC
+                controller.image = imageArray[indexPath.row]
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.backgroundColor = colorArray[indexPath.row]
+        cell.textLabel!.text = namesArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return (tableView.frame.height - (navigationController?.navigationBar.frame.height)!) / CGFloat(imageArray.count)
+    }
+
+
+
+
+}
+
