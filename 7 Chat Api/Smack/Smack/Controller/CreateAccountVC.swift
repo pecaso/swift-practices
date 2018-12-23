4b49b22dc34d00d38b2b5844497713944006c0cd
yasinkbas <yasin.kbas12@gmail.com>
2018-12-23 16:16:43 +0300

    added roundedbuttonsubview && completed register acount

--- 7 Chat Api/Smack/Smack/Controller/CreateAccountVC.swift
+++ 7 Chat Api/Smack/Smack/Controller/CreateAccountVC.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +26,23 @@ class CreateAccountVC: UIViewController {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
+    @IBAction func createAccntPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let pass = passwordText.text , passwordText.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user!")
+            }
+        }
+        
+    }
     
-
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
+    }
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    
 
 }
