71895acaf4810d6f300b18e5111e2d9e7dea60da
yasinkbas <yasin.kbas12@gmail.com>
2019-02-08 16:49:16 +0300

    __:

---
+++ 2 Core IOS Auto Layout&Segues/window-shopper2/window-shopper2Tests/window_shopper2Tests.swift
@@ -0,0 +1,37 @@
+//
+//  window_shopper2Tests.swift
+//  window-shopper2Tests
+//
+//  Created by yasinkbas on 8.02.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import XCTest
+
+class window_shopper2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    func testGetHours() {
+        XCTAssert(Wage.getHours(forWage: 25, andPrice: 100) == 4)
+        XCTAssert(Wage.getHours(forWage: 15.50 ,andPrice: 250.53) == 17)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
