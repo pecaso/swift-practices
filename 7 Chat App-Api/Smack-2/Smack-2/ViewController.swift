d9b72aec7b571edfef2286c39daf50e95a4a1df8
yasinkbas <yasin.kbas12@gmail.com>
2019-02-18 22:47:53 +0300

    Smack-2 initial commit

---
+++ 7 Chat App-Api/Smack-2/Smack-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack-2
+//
+//  Created by yasinkbas on 18.02.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
