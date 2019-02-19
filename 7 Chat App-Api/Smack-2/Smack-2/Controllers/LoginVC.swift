b6e8d56a50739f3fd5c3b6ed1aa53c677414ede7
yasinkbas <yasin.kbas12@gmail.com>
2019-02-19 15:02:48 +0300

    smack-2 Login VC designed

---
+++ 7 Chat App-Api/Smack-2/Smack-2/Controllers/LoginVC.swift
@@ -0,0 +1,33 @@
+//
+//  LoginVC.swift
+//  Smack-2
+//
+//  Created by yasinkbas on 19.02.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 7 Chat App-Api/Smack-2/Smack-2/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack-2
+//
+//  Created by yasinkbas on 19.02.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import Foundation
+//Segues
+let TO_LOGIN = "toLogin"
