65d77d798286cadd073016167b2ae767ea1b696e
yasinkbas <yasin.kbas12@gmail.com>
2019-02-23 18:04:25 +0300

    smack2 registering user

--- 7 Chat App-Api/Smack-2/Smack-2/Controllers/CreateAccountVC.swift
+++ 7 Chat App-Api/Smack-2/Smack-2/Controllers/CreateAccountVC.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,5 +26,22 @@ class CreateAccountVC: UIViewController {
         performSegue(withIdentifier: TO_UNWIND_CHANNELS, sender: nil)
     }
     
-
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else {return}
+        guard let pass = passwordTxt.text , passwordTxt.text != "" else {return}
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("register user!")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func pickBackgroundColorPressed(_ sender: Any) {
+        
+    }
+    
 }
