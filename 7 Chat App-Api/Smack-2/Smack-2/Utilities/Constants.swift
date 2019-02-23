ff0a3822566a951b69e9d94472e5878bf8f18657
yasinkbas <yasin.kbas12@gmail.com>
2019-02-23 17:12:45 +0300

    smack-2 created web request

--- 7 Chat App-Api/Smack-2/Smack-2/Utilities/Constants.swift
+++ 7 Chat App-Api/Smack-2/Smack-2/Utilities/Constants.swift
@@ -7,7 +7,19 @@
 //
 
 import Foundation
-//Segues
+
+//typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://smack-app2.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let TO_UNWIND_CHANNELS = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
