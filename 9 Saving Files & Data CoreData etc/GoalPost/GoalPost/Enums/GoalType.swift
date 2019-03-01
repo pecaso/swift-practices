5af3fd8e7ccaa5846fd90ab59d1ae1998e06e50a
yasinkbas <yasin.kbas12@gmail.com>
2019-03-01 20:27:19 +0300

    finished GoalPost

---
+++ 9 Saving Files & Data CoreData etc/GoalPost/GoalPost/Enums/GoalType.swift
@@ -0,0 +1,15 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by yasinkbas on 28.02.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import Foundation
+
+// .rawValue is String values
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
---
+++ 9 Saving Files & Data CoreData etc/GoalPost/GoalPost/Extensions/UIButtonExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIButtonExtension.swift
+//  GoalPost
+//
+//  Created by yasinkbas on 28.02.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func setSelectedAlpha() {
+        self.alpha = 1
+    }
+    func setDeselectedAlpha() {
+        self.alpha = 0.75
+        
+    }
+}
