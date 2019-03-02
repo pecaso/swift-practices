023e7a020a0e714e579e41118da773959eafab34
yasinkbas <yasin.kbas12@gmail.com>
2019-03-02 17:52:57 +0300

    startrunVC UI done

--- 12 RealmDB/Treads/Treads/BeginRunVC.swift
+++ 12 RealmDB/Treads/Treads/BeginRunVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  BeginRunVC.swift
 //  Treads
 //
 //  Created by yasinkbas on 2.03.2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class BeginRunVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
