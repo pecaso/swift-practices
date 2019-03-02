8d0ab6c0a49607b22d07ce89b26bba465b8ee5a7
yasinkbas <yasin.kbas12@gmail.com>
2019-03-02 17:30:00 +0300

    initial treads project

---
+++ 12 RealmDB/Treads/Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by yasinkbas on 2.03.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 12 RealmDB/Treads/Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by yasinkbas on 2.03.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
