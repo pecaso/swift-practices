85f42edfcfd65d5a6e1bdbbfcb28f8b9183534fb
yasinkbas <yasin.kbas12@gmail.com>
2019-03-07 22:29:36 +0300

    first attemt to CLLocation

--- 12 RealmDB/Treads/Treads/Controllers/BeginRunVC.swift
+++ 12 RealmDB/Treads/Treads/Controllers/BeginRunVC.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class BeginRunVC: UIViewController {
-
+class BeginRunVC: LocationVC {
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        checkLocationAuthStatus()
+        mapView.delegate = self
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.startUpdatingLocation()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        manager?.stopUpdatingLocation()
+    }
+    
+    @IBAction func locationCenterButtonPressed(_ sender: Any) {
+        
+    }
 }
 
+extension BeginRunVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkLocationAuthStatus()
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
+    }
+}
