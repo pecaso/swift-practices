2eb90e4328f2f8953047caa75b6f2634df4c7b9d
yasinkbas <yasin.kbas12@gmail.com>
2019-03-18 22:28:31 +0300

    init breakpoint

---
+++ 13 Firebase/breakpoint/breakpoint/Controllers/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  breakpoint
+//
+//  Created by yasinkbas on 18.03.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
