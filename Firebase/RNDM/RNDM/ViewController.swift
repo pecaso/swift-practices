b522d3ba18aa8738c764a4cb6a3b119cfb1cec13
yasinkbas <yasin.kbas12@gmail.com>
2019-03-28 00:35:22 +0300

    _-_

---
+++ Firebase/RNDM/RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by yasinkbas on 26.03.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Firebase/breakpoint/breakpoint/Views/FeedCell.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCell.swift
+//  breakpoint
+//
+//  Created by yasinkbas on 25.03.2019.
+//  Copyright © 2019 yasinkbas. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLabel.text = email
+        self.contentLabel.text = content
+    }
+    
+    
+
+}
